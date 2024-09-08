431ca6952446c512fdab8d88313d8683f650a46e
Sergey <sinzem@gmail.com>
2024-09-08 22:50:58 +0300

    start

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+// import "./header.scss";
+import "../../styles/style.scss";
+
+const Header = () => {
+    return (
+        <header className="header">
+            Hello World
+        </header>
+    );
+};
+
+export default Header;
